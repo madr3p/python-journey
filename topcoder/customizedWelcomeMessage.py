8f330b515d0b25a0227df015f0e62b59ebb94787
madr3p <144040900+madr3p@users.noreply.github.com>
2025-08-09 01:01:25 +0800

    Update customizedWelcomeMessage.py

--- topcoder/customizedWelcomeMessage.py
+++ topcoder/customizedWelcomeMessage.py
@@ -1,2 +1,11 @@
 name = input("Enter your name\n")
 print("Hello", name, "! Welcome to Amphi Event Management System" )
+
+'''
+Sample Input:
+Enter your name
+Beena
+
+Sample Output:
+Hello Beena ! Welcome to Amphi Event Management System
+'''
